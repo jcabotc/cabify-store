b8e9a688c32b59ff3644b1e87f9c2cbea5ee5747
Jaime Cabot Campins <jcabot@gmail.com>
2017-04-03 14:09:52 +0200

    Challenge examples

--- apps/scanner/config/config.exs
+++ apps/scanner/config/config.exs
@@ -1,1 +1,24 @@
 use Mix.Config
+
+alias Purchase.Promotion.{BuyNPayM, BulkDiscount}
+
+config :purchase, :rules,
+  products: [
+    voucher: [name: "Cabify Voucher",    price: 5.0],
+    tshirt:  [name: "Cabify T-Shirt",    price: 20.0],
+    mug:     [name: "Cabify Coffee Mug", price: 7.5]],
+  promotions: [
+    {BuyNPayM, name:       "2-for-1 on vouchers",
+               product_id: :voucher,
+               buy:        2,
+               pay:        1},
+    {BulkDiscount, name:       "3 or more T-shirts for 19€ per unit ",
+                   product_id: :tshirt,
+                   quantity:   3,
+                   bulk_price: 19.0}]
+
+config :scanner, :codes, %{
+  "VOUCHER" => :voucher,
+  "TSHIRT"  => :tshirt,
+  "MUG"     => :mug
+}
