ac93c7aeaef0012a0743243463bb15bf630384c2
Jaime Cabot Campins <jcabot@gmail.com>
2017-04-03 10:38:33 +0200

    Checkout ready (not consolidating protocols on test env to be able to build implementations on tests)

--- apps/checkout/test/checkout_test.exs
+++ apps/checkout/test/checkout_test.exs
@@ -1,4 +1,38 @@
 defmodule CheckoutTest do
   use ExUnit.Case, async: true
 
+  alias Checkout.{Product, Rules}
+
+  defmodule TestPromotion do
+    defstruct [:discount_amount]
+  end
+
+  defimpl Checkout.Promotion, for: TestPromotion do
+    alias Checkout.{Discount, Bill}
+
+    def apply(%{discount_amount: amount}, bill) do
+      discount = Discount.new(name: "test", amount: amount)
+      Bill.apply(bill, discount)
+    end
+  end
+
+  test "add/2 and bill/1" do
+    product_1 = Product.new(:foo, name: "foo", price: 10.0)
+    product_2 = Product.new(:bar, name: "bar", price: 15.0)
+    products  = [product_1, product_2]
+
+    promotion_1 = %TestPromotion{discount_amount: 2.0}
+    promotion_2 = %TestPromotion{discount_amount: 4.0}
+    promotions  = [promotion_1, promotion_2]
+
+    rules    = Rules.new(products, promotions)
+    checkout = Checkout.new(rules)
+
+    assert {:ok, checkout} = Checkout.add(checkout, :bar)
+    assert {:ok, checkout} = Checkout.add(checkout, :bar)
+    assert {:ok, checkout} = Checkout.add(checkout, :foo)
+
+    bill = Checkout.bill(checkout)
+    assert bill.total == 15.0 + 15.0 + 10.0 - 2.0 - 4.0
+  end
 end
