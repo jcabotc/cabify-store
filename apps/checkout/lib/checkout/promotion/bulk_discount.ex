19f731c88bcf64a506c9018a289a9d54ff2877d2
Jaime Cabot Campins <jcabot@gmail.com>
2017-04-03 10:08:40 +0200

    BulkDiscount implementation of the Promotion protocol

--- apps/checkout/lib/checkout/promotion/bulk_discount.ex
+++ apps/checkout/lib/checkout/promotion/bulk_discount.ex
@@ -25,3 +25,29 @@ defmodule Checkout.Promotion.BulkDiscount do
                   discount_per_unit: discount_per_unit}
   end
 end
+
+defimpl Checkout.Promotion, for: Checkout.Promotion.BulkDiscount do
+  alias Checkout.{Discount, Bill}
+
+  def apply(promotion, %Bill{} = bill) do
+    bill
+    |> Bill.products
+    |> occurrences(promotion.product)
+    |> discount(bill, promotion)
+  end
+
+  defp occurrences(products, target_product) do
+    Enum.count(products, &(&1 == target_product))
+  end
+
+  defp discount(occurrences, bill, %{quantity: quantity})
+  when occurrences < quantity do
+    bill
+  end
+  defp discount(occurrences, bill, %{name: name, discount_per_unit: discount_per_unit}) do
+    amount   = occurrences * discount_per_unit
+    discount = Discount.new(name: name, amount: amount)
+
+    Bill.apply(bill, discount)
+  end
+end
--- apps/checkout/test/checkout/promotion/bulk_discount_test.exs
+++ apps/checkout/test/checkout/promotion/bulk_discount_test.exs
@@ -1,7 +1,7 @@
 defmodule Checkout.Promotion.BulkDiscountTest do
   use ExUnit.Case, async: true
 
-  alias Checkout.{Product, Promotion}
+  alias Checkout.{Product, Discount, Basket, Bill, Promotion}
 
   test "new/4" do
     name      = "3+ foo discount"
@@ -12,5 +12,43 @@ defmodule Checkout.Promotion.BulkDiscountTest do
     assert promotion.product           == product
     assert promotion.quantity          == 3
     assert promotion.discount_per_unit == 2.0
+  end
+
+  describe "Checkout.Promotion implementation" do
+    test "promotion not applicable" do
+      name      = "3+ foo discount"
+      product   = Product.new(:foo, name: "foo", price: 10.0)
+      another   = Product.new(:bar, name: "bar", price: 15.0)
+      promotion = Promotion.BulkDiscount.new(name, product, 3, 2.0)
+
+      bill = Basket.new
+             |> Basket.add(product)
+             |> Basket.add(product)
+             |> Basket.add(another)
+             |> Bill.new
+
+      new_bill = Promotion.apply(promotion, bill)
+      assert new_bill.discounts == []
+    end
+
+    test "promotion applicable" do
+      name      = "2+ foo discount"
+      product   = Product.new(:foo, name: "foo", price: 10.0)
+      another   = Product.new(:bar, name: "bar", price: 15.0)
+      promotion = Promotion.BulkDiscount.new(name, product, 2, 2.0)
+
+      bill = Basket.new
+             |> Basket.add(product)
+             |> Basket.add(another)
+             |> Basket.add(another)
+             |> Basket.add(product)
+             |> Basket.add(product)
+             |> Bill.new
+
+      new_bill = Promotion.apply(promotion, bill)
+
+      expected_discount = Discount.new(name: name, amount: 3 * 2.0)
+      assert new_bill.discounts == [expected_discount]
+    end
   end
 end
